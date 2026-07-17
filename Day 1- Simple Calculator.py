5bdff8e223e73a393b2fdfebf145ed60b94e1346
AyushmanRathore <ayushmanrathore08@gmail.com>
2026-07-17 22:10:25 +0530

    Delete Day 1- Simple Calculator.py

--- Day 1- Simple Calculator.py
+++
@@ -1,7 +0,0 @@
-a= int(input("what is the first number to operate"))
-b= int(input("what is the second number to operate"))
-
-print("The Addition of the two numbers is =", a+b)
-print("The Subtraction of the two numbers is =", a-b)
-print("The Multiplicatin of the two numbers is =", a*b)
-print("The Division of the two numbers is =", a//b)
